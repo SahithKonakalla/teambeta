110268dc79afeb850db902d9f0d029351519c867
Joshua <tweeterfriend@gmail.com>
2016-10-29 14:14:26 -0700

    Target class stuff

--- Target.cpp
+++ Target.cpp
@@ -24,7 +24,7 @@ CannyImage(Mat image)
   std::vector<Point> approxContour;
 
   for(int i = 0; i < contours.size(); i++) //contours.size()??
-  {
+  {10
 
     approxPolyDP(contours[i], approxContour, cv::arcLength(cv::Mat(contours.at(i)), done), done);
     //from the powerpoint
@@ -39,15 +39,70 @@ CannyImage(Mat image)
      imshow("This is the image", img);
     }
 }
+double getMaxY()
+{
+  cv::Point max(0,0);
+  for(unsigned int i = 0; i < edge.size(); i++)
+  {
+      if(edge[i] > max)
+      {
+          max = edge[i];
+      }
+  }
+  return max
+}
 
-
+double getMinY()
+{
+  cv::Point min(10000,10000);
+  for(unsigned int i = 0; i < edge.size(); i++)
+  {
+      if(edge[i] < min)
+      {
+          min = edge[i];
+      }
+  }
+  return min;
+}
 
 double Target::getHeight()
 {
-    return fabs(getTopPoint().y - getBottomPoint().y);
+
+    double max = getMaxY();
+    double min = getMinY();
+    return max - min;
 }
 
+
+double getMaxX()
+{
+   cv::Point max(10000,10000);
+   for(unsigned int i = 0; i < edge.size(); i++)
+   {
+       if(edge[i].x < max.x)
+       {
+           max = edge[i];
+       }
+   }
+   return max;
+}
+
+double getMinX()
+{
+  cv::Point min(0,0);
+    for(unsigned int i = 0; i < edge.size(); i++)
+    {
+        if(edge[i].x > min.x)
+        {
+            min = edge[i];
+        }
+    }
+return min;
+}
 double Target::getWidth()
 {
-    return fabs(getRightPoint().x - getLeftPoint().x);
+      double max = getMaxX();
+      double min = getMaxY();
+
+      return max - min;
 }
